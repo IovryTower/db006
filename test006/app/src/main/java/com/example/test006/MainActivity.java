849f9db53a2bfa6146911bd0c7574c1eddd5b855
IovryTower <37831111+IovryTower@users.noreply.github.com>
2020-03-06 11:42:46 +0800

    update UI

--- test006/app/src/main/java/com/example/test006/MainActivity.java
+++ test006/app/src/main/java/com/example/test006/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.test006;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText e_name,e_email,e_num;
+        TextView show;
+        e_name=findViewById(R.id.edit_name);
+        e_email=findViewById(R.id.edit_email);
+        e_num=findViewById(R.id.edit_num);
+        show=findViewById(R.id.text_show);
+
+        Button btn_write,btn_read,btn_update,btn_remove;
+        btn_write=findViewById(R.id.btn_write);
+        btn_read=findViewById(R.id.btn_read);
+        btn_update=findViewById(R.id.btn_update);
+        btn_remove=findViewById(R.id.btn_remove);
+
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
